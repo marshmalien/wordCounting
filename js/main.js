d3674b702cff04975ce1d40d81a250c87100422a
Marliana Lara <marliana.lara@gmail.com>
2016-09-28 17:52:18 -0400

    move regular expression outside of the loop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function wordCounter(string) {
   var wordCounts = {};
   var subStringArray = string.split(" ");
+  var cleanWord = "";
 
   subStringArray.forEach(function(word) {
-    if(word.length > 1) {
-      var cleanWord = word.replace(/[.,!:;?()]|'s|'d/,"");
-      if(wordCounts[cleanWord] === undefined) {
+    if (word.length > 1) {
+      cleanWord = word.replace(/[.,!:;?()]|'s|'d/, "");
+      if (wordCounts[cleanWord] === undefined) {
         wordCounts[cleanWord] = 1;
       } else {
         wordCounts[cleanWord] += 1;
